72113755efd08223d04f63810189ef9eae02654b
Tomas Kricensky <st52530@student.upce.cz>
2020-04-20 19:07:27 +0200

    Disable flyway for tests, use in-memory DB

--- src/test/kotlin/cz/st52530/recipes/RecipesApplicationTests.kt
+++ src/test/kotlin/cz/st52530/recipes/RecipesApplicationTests.kt
@@ -1,13 +1,21 @@
 package cz.st52530.recipes
 
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 class RecipesApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Value("\${spring.flyway.enabled}")
+    private var flywayEnabled: Boolean = true
+
+    @Test
+    fun whenContextLoads_thenPropertiesAssignedCorrectly() {
+        assertFalse(flywayEnabled)
+    }
 
 }
