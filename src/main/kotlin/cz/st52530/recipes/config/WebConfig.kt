974d9d3da36246881d4c624121eb28b20b7aef46
Tomas Kricensky <st52530@student.upce.cz>
2020-05-02 17:21:33 +0200

    Fix swagger redirect

--- src/main/kotlin/cz/st52530/recipes/config/WebConfig.kt
+++ src/main/kotlin/cz/st52530/recipes/config/WebConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
